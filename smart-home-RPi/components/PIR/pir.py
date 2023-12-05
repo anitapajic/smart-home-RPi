94cff1df9cebc2ce109d10ac0aa3931a15a76774
Anita Pajic <anitaapajic@gmail.com>
2023-12-05 23:43:55 +0100

    Added storing data for pirs

--- smart-home-RPi/components/PIR/pir.py
+++ smart-home-RPi/components/PIR/pir.py
@@ -1,5 +1,53 @@
 import threading
 from simulators.PIR.pir import simulated_pir
+import time
+import json
+import paho.mqtt.publish as publish
+from broker_settings import HOSTNAME, PORT
+
+
+pir_batch = []
+publish_data_counter = 0
+publish_data_limit = 5
+counter_lock = threading.Lock()
+
+
+def publisher_task(event, pir_batch):
+    global publish_data_counter, publish_data_limit
+    while True:
+        event.wait()
+        with counter_lock:
+            local_pir_batch = pir_batch.copy()
+            publish_data_counter = 0
+            pir_batch.clear()
+        publish.multiple(local_pir_batch, hostname=HOSTNAME, port=PORT)
+        print(f'published {publish_data_limit} pir values')
+        event.clear()
+
+
+publish_event = threading.Event()
+publisher_thread = threading.Thread(target=publisher_task, args=(publish_event, pir_batch,))
+publisher_thread.daemon = True
+publisher_thread.start()
+
+
+def pir_callback(name, print_lock, stop_event, dht_settings, publish_event, movement):
+    global publish_data_counter, publish_data_limit
+
+    movement_payload = {
+        "measurement": "Pirs",
+        "simulated": dht_settings['simulated'],
+        "runs_on": dht_settings["runs_on"],
+        "name": dht_settings["name"],
+        "value": movement
+    }
+
+    with counter_lock:
+        pir_batch.append(('Pirs', json.dumps(movement_payload), 0, True))
+        publish_data_counter += 1
+
+    if publish_data_counter >= publish_data_limit:
+        publish_event.set()
 
 
 def run_RPIR1(settings, threads, stop_event, print_lock):
@@ -7,7 +55,7 @@ def run_RPIR1(settings, threads, stop_event, print_lock):
     pir_name = settings['name']
 
     if settings['simulated']:
-        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event))
+        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event, settings, publish_event, pir_callback))
         pir_thread.start()
         threads.append(pir_thread)
     else:
@@ -23,7 +71,7 @@ def run_RPIR2(settings, threads, stop_event, print_lock):
     pir_name = settings['name']
 
     if settings['simulated']:
-        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event))
+        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event, settings, publish_event, pir_callback))
         pir_thread.start()
         threads.append(pir_thread)
     else:
@@ -38,7 +86,7 @@ def run_DPIR1(settings, threads, stop_event, print_lock):
     pir_name = settings['name']
 
     if settings['simulated']:
-        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event))
+        pir_thread = threading.Thread(target=simulated_pir, args=(pir_name, print_lock, stop_event, settings, publish_event, pir_callback))
         pir_thread.start()
         threads.append(pir_thread)
     else:
--- smart-home-RPi/simulators/PIR/pir.py
+++ smart-home-RPi/simulators/PIR/pir.py
@@ -16,13 +16,15 @@ def simulated_no_motion(name, print_lock):
         print("------------------------------------")
 
 
-def simulated_pir(name, print_lock, stop_event):
+def simulated_pir(name, print_lock, stop_event, settings, publish_event, callback):
     try:
         while True:
             if random.randint(-1, 1) > 0:
                 simulated_motion_detected(name, print_lock)
+                callback(name, print_lock, stop_event, settings, publish_event, 1)
             else:
                 simulated_no_motion(name, print_lock)
+                callback(name, print_lock, stop_event, settings, publish_event, 0)
             time.sleep(3)
             if stop_event.is_set():
                 break
